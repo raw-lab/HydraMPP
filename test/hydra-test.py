a8df68222ae9d4607c9df9240487a8f9f6769772
Jose Figueroa <jlfiguer@uncc.edu>
2024-08-06 09:24:31 -0400

    New test for multiple CPUs

--- test/hydra-test.py
+++ test/hydra-test.py
@@ -14,16 +14,26 @@ def main():
 	print("Running test program for HydraMPP")
 	hydraMPP.init()
 	print("Starting jobs")
-	for i in range(400):
+	for i in range(20):
 		slow_func.remote(1)
+	for i in range(10):
+		slow_func.options(num_cpus=4).remote(1)
 	print("Waiting for jobs")
 	ready,queue = hydraMPP.wait()
 	while queue:
 		if ready:
 			print(hydraMPP.get(ready[0]))
-		ready,queue = hydraMPP.wait()
+		ready,queue = hydraMPP.wait(queue)
 	print(ready, queue)
 	print(hydraMPP.get(ready[0]))
+
+	print("TEST COMPLETE")
+	print("exit in 3")
+	time.sleep(1)
+	print("exit in 2")
+	time.sleep(1)
+	print("exit in 1")
+	time.sleep(1)
 	return 0
 
 
