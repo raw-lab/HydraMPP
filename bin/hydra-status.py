51db450865129933a66321679ce63e95a5ddb259
Jose Figueroa <jlfiguer@uncc.edu>
2024-09-05 15:58:20 -0400

    Output format improve

--- bin/hydra-status.py
+++ bin/hydra-status.py
@@ -18,14 +18,13 @@ sock.settimeout(1)
 
 try:
 	sock.sendto("Status".encode(), (args.address, args.port))
-	#msg,_ = sock.recvfrom(1024)
 	data,addr = sock.recvfrom(65536)
 
 	now,nodes,queue = pickle.loads(data)
 
 	print(f"{now[3]}:{now[4]}:{now[5]}")
 
-	print(f"\nNODES: {sum([n['cpus'] for n in nodes])} / {sum([n['num_cpus'] for n in nodes])}")
+	print(f"\n{'TOTAL CPUS:':<15}{sum([n['cpus'] for n in nodes]):>4} / {sum([n['num_cpus'] for n in nodes])}")
 
 	for node in nodes:
 		print(f"{node['hostname']:<15}{node['cpus']:>4} / {node['num_cpus']}")
@@ -33,10 +32,10 @@ try:
 	print(f"QUEUE:", len(queue))
 	for k,v in queue.items():
 		try:
-			print(f"{k:<4}{v['hostname']:<15}\t{v['finished']}: {v['num_cpus']} | {v['func_name']}")
+			print(f"{k:<4}{str(v['hostname']):<15}\t{v['finished']}: {v['num_cpus']} | {v['func_name']}")
 		except Exception as e:
 			printlog("ERROR:", k)
-			printlog(e)
+			printerr(e)
 
 except socket.timeout:
 	pass
